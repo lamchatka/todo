43b040da7a372d81df7b3e352dd810ea01b041b9
Maxim Rukoleev <maxprolife@icloud.com>
2026-02-26 08:38:19 +0300

    Подключим хук useState и научим компонент хранить данные внутри себя. Объясню, как работает состояние в React и почему нельзя использовать обычные переменные. Перепишем переменную tasks на useState, чтобы To Do List стал «живым». Добавим возможность вводить текст новой задачи и сохранять его в отдельное состояние. Настроим форму добавления задачи: создадим новый объект, добавим его в список и очистим поле ввода. Покажу, как удалять все задачи разом и по одной — через setTasks и методы массивов. Перепишем логику переключения выполнения задачи по чекбоксу.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,41 @@
+import { useState } from "react"
 import AddTaskForm from "../components/AddTaskForm"
 import SearchTaskForm from "../components/SearchTaskForm"
 import TodoInfo from "../components/TodoInfo"
 import TodoList from "../components/TodoList"
 
 const Todo = () => {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {id: 'task-1', title: 'Купить молоко', status: false},
     {id: 'task-2', title: 'Погладить кота', status: true},
-  ];
+  ])
+
+  const [newTaskTitle, setNewTaskTitle] = useState('');
 
   const deleteAllTasks = () => {
-    console.log('Удаляем все задачи!');
+    const isConfirmed = confirm('Are you sure you want to delete all?')
+
+    if (isConfirmed) {
+      setTasks([])
+    }
   }
 
   const deleteTask = (taskId) => {
-    console.log(`Удаляем задачу с id: ${taskId}`);
+    setTasks(
+      tasks.filter((task) => task.id !== taskId)
+    )
   }
 
   const toggleTaskComplete = (taskId, status) => {
-    console.log(`Задача ${taskId} ${status ? 'выполнена' : 'не выполнена'}`)
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === taskId) {
+          return { ...task, status }
+        }
+
+        return task
+      })
+    )
   }
 
   const filterTasks = (query) => {
@@ -26,7 +43,16 @@ const Todo = () => {
   }
 
   const addTask = () => {
-    console.log('Задача добавлена!');
+    if (newTaskTitle.trim().length > 0) {
+      const newTask = {
+        id: crypto?.randomUUID() ?? Date.now().toString(),
+        title: newTaskTitle,
+        status: false,
+      }
+
+      setTasks([...tasks, newTask])
+      setNewTaskTitle('');
+    }
   }
 
   const doneTasks = tasks.filter(({ status }) => status).length;
@@ -34,7 +60,11 @@ const Todo = () => {
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
-      <AddTaskForm addTask={addTask}/>
+      <AddTaskForm
+        addTask={addTask}
+        newTaskTitle={newTaskTitle}
+        setNewTaskTitle={setNewTaskTitle}
+      />
       <SearchTaskForm
         onSearchInput={filterTasks}
       />
